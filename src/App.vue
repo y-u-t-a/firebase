4f3f7a737a083d805abb026f4b2a4dcb47eae294
Yuta Ueno <y.ueno0803@gmail.com>
2019-10-08 01:28:43 +0900

    bootstrap-vue を導入！
    npm install bootstrap-vue --save

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar type="light" variant="light">
+      <b-navbar-brand to="/">Yuta Ueno's Page</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #42b983;
-}
-
-#nav a.router-link-exact-active {
-  color: #2c3e50;
-}
 </style>
