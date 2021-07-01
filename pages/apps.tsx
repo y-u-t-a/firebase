a5081d8b0963e28848189f01e6f4d09d07433d37
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-07-02 02:14:26 +0900

    directory-tree-editor ページを追加

--- pages/apps.tsx
+++ pages/apps.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const Apps = () => {
   return (
     <Layout>
       <h1>This is an apps page</h1>
+      <Link href='/apps/directory-tree-editor'>
+        Directory Tree Editor
+      </Link>
     </Layout>
   )
 }
---
+++ pages/apps/directory-tree-editor.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout'
+
+const DirectoryTreeEditor = () => {
+  return (
+    <Layout>
+      <h1>Directory Tree Editor</h1>
+      
+    </Layout>
+  )
+}
+
+export default DirectoryTreeEditor
